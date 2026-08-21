020ea9f68425473cf383b0859d8bb4312a615bb7
vinothini149 <vinothiniav1409@gmail.com>
2026-08-21 15:33:27 +0530

    PROGRAM 3
    Created a new database 'collegeDB' and defined the 'department' table structure. Added 'Email' and 'PhoneNumber' columns to the 'student' table.

--- solution.sql
+++ solution.sql
@@ -1,1 +1,15 @@
-
+CREATE DATABASE collegeDB;
+USE collegeDB:
+CREATE TABLE department (
+  studentID VARCHAR(5) PRIMARY KEY,
+  studentName VARCHAR(10)NOT NULL,
+  DOB date NOT NULL,
+  gender VARCHAR(10)NOT NULL
+);
+desc department;
+ALTER TABLE student
+ADD(
+  Email VARCHAR(30)
+  PhoneNumber NUMERIC(10)
+);
+desc student;
